2dcb0ce3c286c5d339e17d0010e30084255088d9
mariana.lopez <mariana.lopezm@uanl.edu-mx>
2026-03-03 13:30:36 -0600

    Configura navegación con BottomNavigation y fragments base

--- app/src/main/java/com/example/sistemaprestamosbiblioteca/MainActivity.java
+++ app/src/main/java/com/example/sistemaprestamosbiblioteca/MainActivity.java
@@ -2,23 +2,48 @@ package com.example.sistemaprestamosbiblioteca;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Cargar fragment inicial
+        loadFragment(new InicioFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_inicio) {
+                selectedFragment = new InicioFragment();
+            } else if (item.getItemId() == R.id.nav_prestamos) {
+                selectedFragment = new PrestamosFragment();
+                selectedFragment = new InventarioFragment();
+            }
+
+            return loadFragment(selectedFragment);
         });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
